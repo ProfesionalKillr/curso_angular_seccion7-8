d555fe7ad294ce2b3729445094cdec82b163bd5c
ProfesionalKillr <santipiza421@gmail.com>
2024-03-22 14:53:37 -0500

    proyecto seccion7 y 8

---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePagesComponent } from './pages/home-pages/home-pages.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
+import { SharedModule } from '../shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [
+    HomePagesComponent,
+    SearchBoxComponent,
+    CardListComponent,
+    GifsCardComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule
+  ],
+  exports:[
+    HomePagesComponent
+  ]
+})
+export class GifsModule { }
